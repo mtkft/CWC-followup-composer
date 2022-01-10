b7474c563d9a3a66d936de9325a30aadf036d059
puku-777 <63917881+puku-777@users.noreply.github.com>
2022-01-09 21:27:25 -0500

    Begin transcribing logic for Production version 1
    - that snippet of code I still need to implement
    - more detailed comments than ever seen before in code by this guy, so that posterity and future maintainers can know what's up
    - balkanization of dynamic paragraphs (currently just salutation, energy coaches, with more to follow) with no intervening static text being baked in by the script (these are moving to MS Word); fixed size of array `par` to fit all those dynamics
    - list of participating energy coaches with detection and special treatment for if either of the paid staff took part in the tune-up

--- composer.py
+++ composer.py
@@ -27,37 +27,59 @@ This is free software, and you are welcome to redistribute it
 in accordance with the GNU General Public License, version 3.
 """)
 
+# [' '.join(z.split(' ')[:-1]) for z in name.split(' and ')] for splitting resident names
+
 with open(input('Tune-Up data path (*.csv) >>> '),encoding="utf-8") as tuneups, open('merge.csv','w',encoding="utf-8") as merge:
     read_in=csv.reader(tuneups)
     write_out=csv.writer(merge)
     write_out.writerow(['salutation','par1','par2','par3'])
     for row in read_in:
         if row[0] not in ('Timestamp',''): # first row is a header, last few have weird content
-            par = ['' for z in range(4)] # array will contain paragraphs to send to merge.csv
+            par = ['' for z in range(n)] # array will contain n paragraphs to send to merge.csv
             
-            # compose salutation paragraph
+            # paragraph 0: salutation
             # take contents of field 2 (third one over) and spit it out with the
             # last word taken out (word: has spaces around it)
             # and with formatting "Dear namename,"
             # That doesn't work yet, so I've commented out the line. It takes the whole field for now.
-            par[0] = 'Dear '+' '.join(row[2].strip().split(' ')[:-1]).strip()+','
-            #par[0] = f"Dear {row[2]},"
+            par[0] = f"Dear {' '.join(row[2].strip().split(' ')[:-1]).strip()},"
             
-            # paragraph 1
-            # this is obviously facetious; remove in production script!
-            hometype = 'apartment' if row[8] else 'house'
-            par[1] = f"We visited your {hometype} at {' '.join(row[4:8])}"
-            if hometype=='apartment':
-                par[1] += ', '+row[8] # add a comma, a space, and the apartment number field
-            par[1] += ', and energy coach(es) '+row[12]
-            # logic isn't quite working, but is an example of complex handling
-            #coaches = row[12].replace('"','').split(',') # make list of energy coaches and strip quotation marks
-            #print(str(coaches))
-            #if len(coaches)>1 : # more than one energy coach?
-            #    par[1] += 'es '+', '.join(coaches[:-1])+', and '+coaches[-1]
-            #else: par[1] += ' '+coaches[0]
-            par[1] += ' did some tuning-up.'
-
-            # not bothered to write paragraphs 2 or 3 (expandable!)
+            # paragraph 1: energy coaches involved
+            # grab working copy of names list and strip blanks
+            coaches = par[18:23]; coaches.remove('')
+            # if only one coach...
+            if int(row[17])==1:
+                # if that's a staff member...
+                if row[18] in ('Alane Humrich','Belina Meador'): par[1] = "I"
+                # otherwise...
+                else: par[1] = f"our Volunteer Energy Coach, {row[18]}"
+            # if two...
+            elif int(row[17])==2:
+                # if both Alane and Belina...
+                if coaches == ['Alane Humrich','Belina Meador']: par[1] = "Belina Meador and I"
+                # else if Alane but not Belina...
+                elif ('Alane Humrich' in coaches) and not ('Belina Meador' in coaches):
+                    coaches.remove('Alane Humrich')
+                    par[1] = f"our Volunteer Energy Coach, {''.join(coaches)}, and I"
+                # else if just Belina but not Alane...
+                elif ('Belina Meador' in coaches) and not ('Alane Humrich' in coaches):
+                    coaches.remove('Belina Meador')
+                    par[1] = f"our Volunteer Energy Coach, {''.join(coaches)}, and I"
+                # else if no staff, just volunteers...
+                else:
+                    par[1] = f"our Volunteer Energy Coaches, {' and '.join(coaches)}"
+            # if more...
+            elif int(row[17])>2:
+                # if Alane... TODO: decide wording if Alane AND Belina AND volunteers
+                if ('Alane Humrich' in coaches):
+                    coaches.remove('Alane Humrich') # this will currently class Belina under volunteers, if Belina present
+                    par[1] = f"our Volunteer Energy Coaches, {', '.join(coaches)}, and I"
+                # else if Belina but not Alane...
+                elif ('Belina Meador' in coaches) and not ('Alane Humrich' in coaches): # do we actually need this second check about Alane?
+                    coaches.remove('Belina Meador')
+                    par[1] = f"our Volunteer Energy Coach, {', '.join(coaches)}, and I"
+                # else if no staff, just volunteers...
+                else:
+                    par[1] = f"our Volunteer Energy Coaches, {', '.join(coaches[:-1])}, and coaches[-1]"
 
             write_out.writerow(par) # put the paragraphs in that CSV
